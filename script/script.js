9cf56c50b903022868877f9b7bf628e353350852
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-02-20 00:14:30 -0700

    added switchPlayers

--- script/script.js
+++ script/script.js
@@ -5,18 +5,32 @@ const Player = (marker) => {
 
 const displayController = (() => {
   const gameTiles = document.querySelectorAll(".game-tile");
+  const players = ["X", "O"]
+  let currentPlayer = players[0];
 
-  gameTiles.forEach((tile) => {
+  gameTiles.forEach((tile, index) => {
     tile.addEventListener("click", (e) => {
-     console.log(e);
+      e.target.textContent = currentPlayer;
+      gameboard.boardTiles[index] = currentPlayer;
+      console.log(gameboard.boardTiles);
+      switchPlayers();
     });
   });
+
+  const switchPlayers = () => {
+    if (currentPlayer === players[0]){
+      currentPlayer = players[1];
+      console.log(currentPlayer);
+    } else {
+      currentPlayer = players[0];
+      console.log(currentPlayer);
+    }
+  }
 
   return { gameTiles };
 })();
 
 const gameboard = (() => {
   const boardTiles = Array(displayController.gameTiles.length);
-  return { boardTiles, playGame };
+  return { boardTiles };
 })();
-
