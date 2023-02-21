d3fa82559ada76ac50866237178aab234885102c
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-02-20 23:05:47 -0700

    added players created as objects

--- script/script.js
+++ script/script.js
@@ -6,24 +6,39 @@ const Player = (marker) => {
 
 const displayController = (() => {
   const gameTiles = document.querySelectorAll(".game-tile");
- const players = ["X", "O"]
-
+  const player1 = Player("X");
+  const player2 = Player("O");
+  const players = [player1.getPlayerMarker(), player2.getPlayerMarker()];
   let turn = players[0];
 
   gameTiles.forEach((tile, index) => {
     tile.addEventListener("click", (e) => {
       e.target.textContent = turn;
+      tile.classList.add("restricted");
       gameboard.boardTiles[index] = turn;
-      switchPlayers();
+      switchPlayers(e);
+      checkWinner(gameboard.boardTiles);
     });
   });
 
-  const handleClick = () => {
-
-  }
-
   const switchPlayers = () => {
     turn === players[0] ? (turn = players[1]) : (turn = players[0]);
+  };
+
+  const checkWinner = (boardTiles) => {
+    let winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    boardTiles.forEach((tile, index) => {
+
+    });
   };
 
   return { gameTiles };
