1652134c4564ebdfe73b36006a3a700fdf73e305
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-02-22 00:22:38 -0700

    FINALLY got the winner check feature

--- script/script.js
+++ script/script.js
@@ -9,6 +9,17 @@ const displayController = (() => {
   const player1 = Player("X");
   const player2 = Player("O");
   const players = [player1.getPlayerMarker(), player2.getPlayerMarker()];
+  const winningPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [3, 4, 6],
+  ];
+
   let turn = players[0];
 
   gameTiles.forEach((tile, index) => {
@@ -16,28 +27,43 @@ const displayController = (() => {
       e.target.textContent = turn;
       tile.classList.add("restricted");
       gameboard.boardTiles[index] = turn;
-      checkWinner(turn);
-      switchPlayers(e);
+      checkWinner();
+      switchPlayers();
     });
   });
 
-  const switchPlayers = () => {
-    turn === players[0] ? (turn = players[1]) : (turn = players[0]);
+  const checkWinner = () => {
+    let winner = false;
+    for (let i = 0; i < winningPattern.length; i++) {
+      const pattern = winningPattern[i];
+      const cellA = gameboard.boardTiles[pattern[0]];
+      const cellB = gameboard.boardTiles[pattern[1]];
+      const cellC = gameboard.boardTiles[pattern[2]];
+
+      if (
+        cellA === player1.getPlayerMarker() &&
+        cellB === player1.getPlayerMarker() &&
+        cellC === player1.getPlayerMarker()
+      ) {
+        console.log(`Player ${player1.getPlayerMarker()} is the winner`);
+        console.log(pattern);
+        winner = true;
+        break;
+      } else if (
+        cellA === player2.getPlayerMarker() &&
+        cellB === player2.getPlayerMarker() &&
+        cellC === player2.getPlayerMarker()
+      ) {
+        console.log(`Player ${player2.getPlayerMarker()} is the winner`);
+        console.log(pattern);
+      } else {
+        console.log("draw");
+      }
+    }
   };
 
-  const checkWinner = (turn) => {
-    let winPattern = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [3, 4, 6],
-    ];
-
-
+  const switchPlayers = () => {
+    turn === players[0] ? (turn = players[1]) : (turn = players[0]);
   };
 
   return { gameTiles };
@@ -45,5 +71,6 @@ const displayController = (() => {
 
 const gameboard = (() => {
   const boardTiles = Array(displayController.gameTiles.length);
+  console.log(boardTiles);
   return { boardTiles };
 })();
